08e4ae0fbf1b27232ceba269d9d719e7906cf5ef
Megan Carnaghi <carnagh5@gmail.com>
2020-12-20 16:46:33 -0500

    Added user prompt for amt of rain. Added functionality to convert number to asterisks and console.log result.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Create a variable to hold the asterick symbol represening each inch of rain.
+let rainSymbol = " * ";
+
+// Prompt the user for the amount of rain they received (in inches) and store the amount in a javascript variable.
+let rainAmt = prompt(
+  "How much rain did you receive? Please provide the number of inches."
+);
+
+// Use the .repeat method to return the same number asterisks as the number of inches of rain the user said they received. Store in a javascript variable.
+let rainInches = rainSymbol.repeat(rainAmt);
+
+// console.log the asterisks reperesenting the inches of rain received.
+console.log("Inches of rain received: " + rainInches);
