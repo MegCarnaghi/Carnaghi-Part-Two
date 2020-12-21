e65f9ea736a085e18b6e5e4de9c6467c76cdec06
Megan Carnaghi <carnagh5@gmail.com>
2020-12-20 21:44:40 -0500

    Cleaned up global variables, code comments and console log messages.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,11 @@
 // Create global variables
-// A variable to hold the asterick symbol represening each inch of rain.
+// A variable to hold the asterick symbol represening each inch of rain the user enters in the prompt
 let rainSymbol = " * ";
 
 // A variable for the normal amount of bushels of grain the land produces
 let bushels = 50;
 
-// A variable for the users answer to the prompt about whether or not they use fertilizer
-let usedFertilizer;
-
-// A variable for the type of fertilizer the user is using
-let typeFertilizer;
-
-// Prompt the user for the amount of rain they received (in inches) and store the amount in a javascript variable
+// A prompt to ask the user for the amount of rain they received (in inches) and store the amount in a javascript variable
 let rainAmt = prompt(
   "How much rain did you receive? Please provide the number of inches."
 );
@@ -23,15 +17,16 @@ function rainPromptActions() {
       "That is not a valid response. Please try again and make sure to use numbers only."
     );
   } else {
-    // Use the .repeat method to return the same number asterisks as the number of inches of rain the user said they received. Store in a javascript variable
+    // Use the .repeat method to return the same number of asterisks as the number of inches of rain the user said they received and store in a javascript variable
     let rainInches = rainSymbol.repeat(rainAmt);
-    // console log the asterisks reperesenting the inches of rain received
+    // Console log the asterisks reperesenting the inches of rain received
     console.log("Inches of rain received: " + rainInches);
+    // Calling the usedFertilizerPrompt function
     usedFertilizerPrompt();
   }
 }
 
-// Callin the rainPromptActions function
+// Calling the rainPromptActions function
 rainPromptActions();
 
 // A function to determine what actions to do based on the user's response to the prompt about whether or not they use fertilizer
@@ -39,7 +34,7 @@ function usedFertilizerPrompt() {
   let usedFertilizer = prompt(
     "Did you use fertilizer? Please answer yes or no."
   ).toUpperCase();
-  // secnario 1 - the user did not use fertilizer and rainAmt is >= 20 inches
+  // Secnario 1 - The user did not use fertilizer and rainAmt is >= 20 inches
   if (usedFertilizer === "NO" && rainAmt >= 20) {
     alert("Thank you for your responses. Check the console for your answers.");
     let rainImpact = bushels * 0.9;
@@ -48,7 +43,7 @@ function usedFertilizerPrompt() {
         rainImpact +
         " bushels."
     );
-    // scenario 2 - the user did not use fertilizer and rainAmt is <10 inches
+    // Scenario 2 - The user did not use fertilizer and rainAmt is <10 inches
   } else if (usedFertilizer === "NO" && rainAmt < 10) {
     alert("Thank you for your responses. Check the console for your answers.");
     let rainImpact = bushels * 0.8;
@@ -57,7 +52,7 @@ function usedFertilizerPrompt() {
         rainImpact +
         " bushels."
     );
-    // scenario 3 - the user did not use fertilizer and the rainAmt is not <10 inches and not >= 20 inches
+    // Scenario 3 - The user did not use fertilizer and the rainAmt is not <10 inches and not >= 20 inches
   } else if (usedFertilizer === "NO" && rainAmt >= 10 && rainAmt < 20) {
     alert("Thank you for your responses. Check the console for your answers.");
     let rainImpact = bushels;
@@ -65,24 +60,24 @@ function usedFertilizerPrompt() {
       "No fertilizer was used and the rain amount was average. The expected crop yield is " +
         rainImpact +
         " bushels."
-    ); // scenario 4 - the user did use fertzlier
+    ); // Scenario 4 - The user did use fertzlier
   } else if (usedFertilizer === "YES") {
     typeOfFertilizerPrompt();
 
-    // scenario 5 - the user entered an invalid response
+    // Scenario 5 - The user entered an invalid response
   } else {
     alert(
-      "You did not enter a valid response. Please try again and make sure to to answer with yes or no only."
+      "You did not enter a valid response. Please try again and make sure to answer using the words 'yes' or 'no' only."
     );
   }
 }
 
-// A function to determine what actions to do based on the user's response to the prompt about what type of fertilizer they use
+// A function to determine what actions to do based on the user's response to the prompt about what type of fertilizer they used
 function typeOfFertilizerPrompt() {
   let typeFertilizer = prompt(
     "What type of fertilizer did you use - regular or premium?"
   ).toUpperCase();
-  //scenario 1 - regular fertilizer was used and rainAmt is >= 20 inches
+  //Scenario 1 - Regular fertilizer was used and rainAmt is >= 20 inches
   if (typeFertilizer === "REGULAR" && rainAmt >= 20) {
     alert("Thank you for your responses. Check the console for your answers.");
     let rainImpact = bushels * 0.9;
@@ -93,7 +88,7 @@ function typeOfFertilizerPrompt() {
         finalYield +
         " bushels."
     );
-    // secnario 2 - regular fertilizer was used and rainAmt is <10 inches
+    // Secnario 2 - Regular fertilizer was used and rainAmt is <10 inches
   } else if (typeFertilizer === "REGULAR" && rainAmt < 10) {
     alert("Thank you for your responses. Check the console for your answers.");
     let rainImpact = bushels * 0.8;
@@ -104,18 +99,17 @@ function typeOfFertilizerPrompt() {
         finalYield +
         " bushels."
     );
-    // scenario 3 - regular fertilizer was used and the rainAmt is not <10 inches and not >= 20 inches
+    // Scenario 3 - Regular fertilizer was used and the rainAmt is not <10 inches and not >= 20 inches
   } else if (typeFertilizer === "REGULAR" && rainAmt >= 10 && rainAmt < 20) {
     alert("Thank you for your responses. Check the console for your answers.");
     let rainImpact = bushels;
     let finalYield = rainImpact * 0.1 + rainImpact;
-    // let finalYield = rainImpact + perctage;
     console.log(
       typeFertilizer +
         " fertilizer was used and the rain amount was average. The expected crop yield is " +
         finalYield +
         " bushels."
-    ); // sceanrio 4 - premium fertilizer was used and rainAmt is >= 20 inches
+    ); // Sceanrio 4 - Premium fertilizer was used and rainAmt is >= 20 inches
   } else if (typeFertilizer === "PREMIUM" && rainAmt >= 20) {
     alert("Thank you for your responses. Check the console for your answers.");
     let rainImpact = bushels * 0.9;
@@ -125,7 +119,7 @@ function typeOfFertilizerPrompt() {
         " fertilizer was used and there was a lot of rain. The expected crop yield is " +
         finalYield +
         " bushels."
-    ); // scenario 5 - premium fertilizer was used and rainAmt is <10 inches
+    ); // Scenario 5 - Premium fertilizer was used and rainAmt is <10 inches
   } else if (typeFertilizer === "PREMIUM" && rainAmt < 10) {
     alert("Thank you for your responses. Check the console for your answers.");
     let rainImpact = bushels * 0.8;
@@ -136,7 +130,7 @@ function typeOfFertilizerPrompt() {
         finalYield +
         " bushels."
     );
-    // scenario 6 - premium fertilizer was used and the rainAmt is not <10 inches and not >= 20 inches
+    // Scenario 6 - Premium fertilizer was used and the rainAmt is not <10 inches and not >= 20 inches
   } else if (typeFertilizer === "PREMIUM" && rainAmt >= 10 && rainAmt < 20) {
     alert("Thank you for your responses. Check the console for your answers.");
     let rainImpact = bushels;
@@ -146,10 +140,10 @@ function typeOfFertilizerPrompt() {
         " fertilizer was used and the rain amount was average. The expected crop yield is " +
         finalYield +
         " bushels."
-    ); // scenario 7 - invalid response is given
+    ); // Scenario 7 - The user entered an invalid response
   } else {
     alert(
-      "You did not enter a valid response. Please try again and make sure to answer with regular or premium only."
+      "You did not enter a valid response. Please try again and make sure to answer using the words 'regular' or 'premium' only."
     );
   }
 }
